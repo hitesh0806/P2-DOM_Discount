7dd0073bfa308ef5856ac3b58356d7f2f2249524
hitesh0806 <hiteshtg812@gmail.com>
2025-04-10 13:00:35 +0530

    Added tax

--- java.js
+++ java.js
@@ -3,25 +3,28 @@ function calculate()
     let inp=document.querySelector('.i1').value;
     let x;
     let y;
+    let z;
     let a='';
     console.log(typeof inp);
     console.log(typeof x);
     console.log(typeof y);
+    z=((10/1000)*inp);
     if(inp>=5000)
     {
-        x=inp-((25/100)*inp);
-        y=inp-x;
+        x=inp-((25/100)*inp)+z;
+        y=inp-(inp-((25/100)*inp));
         a="Since you've purchased items worth more than 5000/-, you'll get a discount of 25%";
     }
     else
     {
-        x=inp;
+        x=inp+z;
         y=0;
         a="Since you've purchased items worth less than 5000/-, you'll not get any discount";
     }
     document.querySelector('.h1').innerHTML="Total amount: "+inp;
     document.querySelector('.h2').innerHTML="Discount amount: "+y;
-    document.querySelector('.h3').innerHTML="Final amount: "+x;
-    document.querySelector('.h4').innerHTML=a;
+    document.querySelector('.h3').innerHTML="Tax(10%): "+z;
+    document.querySelector('.h4').innerHTML="Final amount: "+x;
+    document.querySelector('.h5').innerHTML=a;
 
 }
