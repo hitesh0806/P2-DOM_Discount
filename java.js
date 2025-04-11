9422b17ce32b45260ca51dfa62351270a5b0264b
hitesh0806 <hiteshtg812@gmail.com>
2025-04-11 19:59:53 +0530

    Added GST,CGST,SGST

--- java.js
+++ java.js
@@ -4,27 +4,40 @@ function calculate()
     let x;
     let y;
     let z;
+    let b;
+    let c;
+    let e;
+    let f;
+    let d;
     let a='';
     console.log(typeof inp);
     console.log(typeof x);
     console.log(typeof y);
-    z=((10/1000)*inp);
+    z=((10/100)*inp);
+    b=((6/100)*inp);
+    c=((4/100)*inp);
+    e=((2/100)*inp);
+    f=((25/100)*inp);
+    d=z+b+e+c;
     if(inp>=5000)
     {
-        x=inp-((25/100)*inp)+z;
-        y=inp-(inp-((25/100)*inp));
+        x=inp-f+d;
+        y=((25/100)*inp);
         a="Since you've purchased items worth more than 5000/-, you'll get a discount of 25%";
     }
     else
     {
-        x=inp+z;
+        x=inp+d;
         y=0;
         a="Since you've purchased items worth less than 5000/-, you'll not get any discount";
     }
     document.querySelector('.h1').innerHTML="Total amount: "+inp;
-    document.querySelector('.h2').innerHTML="Discount amount: "+y;
+    document.querySelector('.h2').innerHTML="Discount(25%): "+y;
     document.querySelector('.h3').innerHTML="Tax(10%): "+z;
-    document.querySelector('.h4').innerHTML="Final amount: "+x;
-    document.querySelector('.h5').innerHTML=a;
+    document.querySelector('.h4').innerHTML="GST(6%): "+b;
+    document.querySelector('.h5').innerHTML="SGST(4%): "+c;
+    document.querySelector('.h6').innerHTML="CGST(2%): "+e;
+    document.querySelector('.h7').innerHTML="Final amount: "+x;
+    document.querySelector('.h8').innerHTML=a;
 
 }
